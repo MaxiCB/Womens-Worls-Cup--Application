97b5b0f1ba27dd2febecba6c30525b948cfd65e1
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-14 10:42:35 -0600

    ADD React-Router-Dom && Base data fetching from API
    - Installed React-Router-Dom in project
    - Implemented fetching data from API within App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+// Router
+import {Route, BrowserRoute as Router} from 'react-router-dom';
+// Axios
+import axios from 'axios';
+// Custom Hooks
+import { useLocalStorage} from './hooks/useLocalStorage';
+// Components
 
-function App() {
+
+const App = () => {
+
+  const [playerData, setPlayerData] = useState();
+
+  useEffect(() => {
+    // Implement axios call here
+    // {country, id, name, searches}
+    axios.get('http://localhost:5000/api/players')
+      .then(res => setPlayerData(res.data))
+      .catch(err => console.log(err))
+  })
+
   return (
     <div className="App">
-      
+
     </div>
   );
 }
